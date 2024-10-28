2e55993cc9bf6a028275428e0b4836fd68dd6894
Gabriel Souza <paixaogabriel498@gmail.com>
2024-10-28 15:02:02 -0300

    cklsncaksncac

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -7,18 +7,8 @@ Office.onReady(function (info) {
 
 // Define a função para carregar a página
 function carregarPagina() {
-  var url = "https://gabrieups.github.io/Addin/src/taskpane/taskpane.html";
-
-  Office.context.ui.displayDialogAsync(
-    url,
-    { height: 100, width: 50 },
-    function (asyncResult) {
-      if (asyncResult.status === Office.AsyncResultStatus.Failed) {
-        console.error("Erro ao carregar a página: " + asyncResult.error.message);
-      } else {
-        console.log("Página carregada com sucesso.");
-      }
-    }
+    Office.context.ui.displayDialogAsync('https://gabrieups.github.io/Addin/src/taskpane/taskpane.html',
+      {height: 30, width: 20, promptBeforeOpen: false, displayInIframe: true}
   );
 }
 
