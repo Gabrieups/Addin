01dbd7d5a9a76b83a08932f3c6d54835c4c3afe8
Gabriel Souza <paixaogabriel498@gmail.com>
2024-10-29 12:00:08 -0300

    ausfohfiasfa

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -1,12 +1,17 @@
-function ALterarMapa(){
-  var mapaTerreo = document.getElementById("mapaTerreo");
-  var mapaPrimeriro = document.getElementById("mapaPrimeiro");
+(function(){
+  const changeMap = document.getElementById("MudaMapa");
 
-  if(mapaTerreo.style.display === "block"){
-    mapaTerreo.style.display = "none";
-    mapaPrimeriro.style.display = "block";
-  } else {
-    mapaTerreo.style.display = "block";
-    mapaPrimeriro.style.display = "none";
+  function ALterarMapa(){
+    var mapaTerreo = document.getElementById("mapaTerreo");
+    var mapaPrimeriro = document.getElementById("mapaPrimeiro");
+
+    if(mapaTerreo.style.display === "block"){
+      mapaTerreo.style.display = "none";
+      mapaPrimeriro.style.display = "block";
+    } else {
+      mapaTerreo.style.display = "block";
+      mapaPrimeriro.style.display = "none";
+    }
   }
-}
+  changeMap.addEventListener('click', ALterarMapa);
+})();
