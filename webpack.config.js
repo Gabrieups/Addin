22658684ec06b1ca1a65b65bbf7c88c44dc451e3
Gabriel Souza <paixaogabriel498@gmail.com>
2024-12-19 00:56:25 -0300

    wesdfsdf

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
-const urlDev = "https://breu-app-testing-bhcmcdadfqdthzhy.brazilsouth-01.azurewebsites.net/";
+const urlDev = "https://localhost:3000/";
 const urlProd = "https://breu-app-testing-bhcmcdadfqdthzhy.brazilsouth-01.azurewebsites.net/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
 
 async function getHttpsOptions() {
