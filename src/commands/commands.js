2e8c859e7ddff1a74efeeff9389198ed0350f992
Gabriel Souza <paixaogabriel498@gmail.com>
2025-01-07 13:36:46 -0300

    asofjapfnajfkajfn

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -6,13 +6,15 @@ Office.initialize = function () {
 let settingsDialog;
 
 function carregarPagina(event) {
-  btnEvent = event;
-  const url = new URI('dialog.html?warn=1').absoluteTo(window.location).toString();
-  const dialogOptions = { width: 60, height: 80 };
-
-  Office.context.ui.displayDialogAsync(url, dialogOptions, function(result) {
-    settingsDialog = result.value;
-    settingsDialog.addEventHandler(Office.EventType.DialogEventReceived, dialogClosed);
+  getSSOToken().then((token) =>{
+    btnEvent = event;
+    const url = new URI(`dialog.html?warn=1&token=${token}`).absoluteTo(window.location).toString();
+    const dialogOptions = { width: 60, height: 80 };
+  
+    Office.context.ui.displayDialogAsync(url, dialogOptions, function(result) {
+      settingsDialog = result.value;
+      settingsDialog.addEventHandler(Office.EventType.DialogEventReceived, dialogClosed);
+    })
   });
 }
 
@@ -24,3 +26,35 @@ function dialogClosed(message) {
   btnEvent.completed();
   btnEvent = null;
 }
+
+async function getSSOToken(){
+  try {
+    const accessToken = await OfficeRuntime.auth.getAccessToken({allowConsentPrompt: true, allowSignInPrompt: true, forMSGraphAccess: true});
+    console.log("Token obtido com sucesso:", accessToken);
+    return accessToken;
+
+  } catch (exception) {
+    console.log(`Failed to get token. Error: ${JSON.stringify(exception)}`);
+  }
+}
+
+// async function sendToken(token) {
+//   try {
+//       const response = await fetch('https://localhost:44339/api/UserData/user-data', {
+//           method: 'GET',
+//           headers: {
+//               'Content-Type': 'application/json',
+//           },
+//           body: JSON.stringify({ token }),
+//       });
+
+//       if (response.ok) {
+//           const data = await response.json();
+//           console.log("Resposta do backend:", data);
+//       } else {
+//           console.error("Erro ao enviar token para o backend:", response.statusText);
+//       }
+//   } catch (error) {
+//       console.error("Erro na requisição:", error);
+//   }
+// }
--- src/settings/dialog.js
+++ src/settings/dialog.js
@@ -1,15 +1,48 @@
-Office.initialize(function (){
-    getSSOToken();
-});
+// Office.onReady((info) => {
+//     if (info.host === Office.HostType.Outlook) {
+//         getSSOToken();
+//     }
+// });
 
-function getSSOToken(){
-    OfficeRuntime.auth.getAccessToken({allowSignInPrompt: true})
-    .then((token) => {
-        return fetch("https://localhost:44339/api/user/user-data", {
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
-    }).then(response => response.json())
-}
+
+// function getSSOToken(){
+//     try{
+//         Office.auth.getAccessToken({allowSignInPrompt: true, forMSGraphAccess: true, allowConsentPrompt: true }, (result) => {
+
+//             console.log("ó o resultado:", result)
+//             if(result.status === Office.AsyncResultStatus.Succeeded){
+//                 const accessToken = result.value;
+    
+//                 console.log("ó o token:", token)
+                
+//                 sendToken(accessToken);
+//             } else {
+//                 console.error(result.error.message);
+//             }
+//         });
+//     }
+//     catch (exception) {
+//         console.log(`Failed to get token. Error: ${JSON.stringify(exception)}`);
+//     }
+// }
+
+// async function sendToken(token) {
+//     try {
+//         const response = await fetch('https://localhost:44339/api/UserData/user-data', {
+//             method: 'GET',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify({ token }),
+//         });
+
+//         if (response.ok) {
+//             const data = await response.json();
+//             console.log("Resposta do backend:", data);
+//         } else {
+//             console.error("Erro ao enviar token para o backend:", response.statusText);
+//         }
+//     } catch (error) {
+//         console.error("Erro na requisição:", error);
+//     }
+// }
